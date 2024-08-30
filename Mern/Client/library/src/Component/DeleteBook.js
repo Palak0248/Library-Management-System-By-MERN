77c245379c03fc153c646e2cee419faef54377a4
Palak0248 <115970731+Palak0248@users.noreply.github.com>
2024-08-30 09:01:27 +0530

    Add files via upload

---
+++ Mern/Client/library/src/Component/DeleteBook.js
@@ -0,0 +1,4 @@
+export default function DeleteBook()
+{
+    return(<>Delete Book </>);
+}
---
+++ Mern/Server/routes/issueReturn.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const router=express.Router();
+const Books=require("../Model/books");
+const Students=require("../Model/students");
+
+router.get("/",async (req,res)=>{
+
+});
+
+
+module.exports=router;
